0f7a61e3ef304a46025ce73c066ba24d64fe870f
Luke Pearson <lukepearson@live.com>
2016-02-16 22:23:52 +1000

    More work, should ideally be testable tomorrow.

--- ContextSpamSubmission/WorkLogic.cs
+++ ContextSpamSubmission/WorkLogic.cs
@@ -13,26 +13,34 @@ namespace ContextSpamSubmission
     {
         //variable declarations XXX
         //the registry hive containing our address keys.
-        String regPath = "HKEY_CURRENT_USER\\SOFTWARE\\ElementalSoftware\\";
+        string regPath = "HKEY_CURRENT_USER\\SOFTWARE\\ElementalSoftware\\SpamSubmission\\";
         //the key containing the ticket 'voicemail' address. Emailing this address should result in
         //a ticket being created, with a reference to the SPAM sample.
-        String regTicketAddress = "ticketsEmail";
+        string regTicketAddress = "ticketEmail";
         //the key containing the address we submit the SPAM sample to.
-        String regSubmitAddress = "spamEmail";
+        string regSubmitAddress = "spamEmail";
+        //key that holds the zip password
+        string regEncryptionPassword = "encryptionPassword";
+        //string to store the registry key holding the debug value
+        string regDebug = "debug";
         //key to hold the ticket voicemail address, once we get it.
-        String emailTicketAddress = "";
+        string emailTicketAddress = "";
         //key to hold the SPAM submission address, once we get it.
-        String spamSubmitAddress = "";
+        string spamSubmitAddress = "";
+        //string to store encryption password
+        string encryptionPassword = "";
+        //A string to hold the interesting items we want to report on in plaintext
+        string metadata = "";
+        //A string to hold the unique identifier of each message
+        string uid = "";
+        //boolean value (stored in reg) dictating wether or not we should show debugging messages
+        bool debug;
+        
         public void submit()
         {
 
 
-            //read in addresses and persistent settings from registry. XXX
-            //ticket voicemail address
-            String temp = regPath + regTicketAddress;
-
-            //safe place to send spam to
-
+            Microsoft.Office.Interop.Outlook.Application outlookApp = new Microsoft.Office.Interop.Outlook.Application();
             //submission zip password
 
             //Main logic, majority of program logic is below, in this method.
@@ -47,19 +55,50 @@ namespace ContextSpamSubmission
                     MailItem badMail = item as MailItem;
                     if (badMail != null)
                     {
-                        //testing message, can likely remove this later. XXX
-                        MessageBox.Show("You've submitted something you think is SPAM!\r\n" +
-                        badMail.Subject, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        //set the uid of this message
+                        //after much deliberation, we'll initialize rand here,
+                        //as it gives us the greatest chance of a non duplicated random number
+                        Random rand = new Random();
+                        uid = rand.Next(100000000, 999999999).ToString();
+                        uid += rand.Next(100000000, 999999999).ToString();
+                        uid += rand.Next(100000000, 999999999).ToString();
 
                         //This will pull out the headers and such, and whack them into variables.
-
+                        metadata = "To: " +  badMail.To + "\r\n";
+                        metadata += "From: " +  badMail.SenderName + ": " + badMail.SenderEmailAddress + "\r\n";
+                        metadata += "Subject: " + badMail.Subject + "\r\n";
+                        metadata += "CC: " + badMail.CC + "\n\r";
+                        metadata += "Companies Associated With Email: " + badMail.Companies + "\r\n";
+                        metadata += "Email Creation Time: " + badMail.CreationTime + "\r\n";
+                        metadata += "Delivery Report Requested: " +badMail.OriginatorDeliveryReportRequested + "\r\n";
+                        metadata += "Received Time: " + badMail.ReceivedTime + "\r\n";
+                        metadata += "Sent On: " + badMail.SentOn.ToString() + "\r\n";
+                        metadata += "Size (kb): " + ((badMail.Size)/1024).ToString() + "\r\n";
+                        metadata += "Plaintext Body: \r\n" + badMail.Body + "\r\n";
 
                         //This will create a mail item, and send it to the designated mailbox of a ticketing system.
+                        Microsoft.Office.Interop.Outlook.MailItem ticketMail = (Microsoft.Office.Interop.Outlook.MailItem) outlookApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem);
+                        ticketMail.To = emailTicketAddress;
+                        ticketMail.Subject = uid;
+                        ticketMail.Body = metadata;
+                        ticketMail.Send();
+
 
 
                         //This will create a mail item, and send it to a sample collection mailbox, with the badSample attached.
+                        Microsoft.Office.Interop.Outlook.MailItem spamMail = (Microsoft.Office.Interop.Outlook.MailItem)outlookApp.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem);
+                        spamMail.To = spamSubmitAddress;
+                        spamMail.Subject = uid;
+                        spamMail.Body = metadata;
+                        //spamMail.a
+                        spamMail.Send();
 
-
+                        //testing message, can likely remove this later. XXX
+                        if (debug)
+                        {
+                            MessageBox.Show("You've submitted something you think is SPAM!\r\n" +
+                        metadata, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
                     }
                     //Not a mail item, need to decide how to handle this. Advise user they done goofed. XXX
                     else
@@ -74,15 +113,20 @@ namespace ContextSpamSubmission
         public bool initialize()
         {
             //debug string for testing
-            String debug = "";
+            string debugMessage= "";
             //let's grab our stuff from the registry
             try
             {
+                debug = Registry.GetValue(regPath, regDebug, "false").ToString().ToLower().Equals("true");
+
                 emailTicketAddress = Registry.GetValue(regPath, regTicketAddress, null).ToString();
-                debug += "Email Ticket Address: " + emailTicketAddress + "\n";
+                debugMessage += "Email Ticket Address: " + emailTicketAddress + "\n";
 
                 spamSubmitAddress = Registry.GetValue(regPath, regSubmitAddress, null).ToString();
-                debug += "Spam Submit Address: " + spamSubmitAddress + "\n";
+                debugMessage += "Spam Submit Address: " + spamSubmitAddress + "\n";
+
+                encryptionPassword = Registry.GetValue(regPath, regEncryptionPassword, null).ToString();
+                debugMessage += "Encryption Password: " + encryptionPassword + "\n";
             }
             catch (System.Exception e)
             {
@@ -91,7 +135,7 @@ namespace ContextSpamSubmission
                     "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
-            MessageBox.Show(debug, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(debugMessage, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             return true; 
         }
     }
